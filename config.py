d564167142ece182d75be5dad053160185843caf
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-06-08 13:30:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ import os
 
 
 class config(object):
-	API_ID = int(os.environ.get("API_ID", "64225"))
-	API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5951446975:AAHomxCgmzt2HAyLF5K5E3nvoZGihYywY4Y")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "PyroStorageBot")
-	CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001851293267"))
+	API_ID = int(os.environ.get("API_ID", "23990433"))
+	API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6119758281:AAFNCl4gzd-1w957bRKEhjb6kW1JnaDZm3U")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "File_To_Links1_Bot")
+	CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001828551401"))
 	SUPPORT_GROUP = os.environ.get("SUPPORT_CHAT", "TheDeadlyBots")
 	SUPPORT_CHANNEL = os.environ.get("SUPPORT_CHANNEL", "TheBotUpdates")
         BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
